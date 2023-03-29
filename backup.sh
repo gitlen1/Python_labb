1a3989c7a64b9120ebc9a8117e88783711eec90b
gitlen1 <113553237+gitlen1@users.noreply.github.com>
2023-03-29 17:09:02 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-source=/users/ilyas/documents/bash/source
-target=/users/ilyas/documents/bash/target
+source=/mnt/c/Users/ilyas/DevOps/Bash/source
+target=vagrant@192.168.56.10:/home/vagrant/target
+
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+backup_dir="$source/../backup_$timestamp"
+mkdir "$backup_dir"
+
+if [[ $target == *":"* ]]; then
+  rsync -avz --delete -e ssh $source/ $target/
+else
+  rsync -avz --delete $source/ $target/
+fi
 
 for file in $(find $source -type f -print | sed "s|$source/||") ; do
     if [ -a $target/$file ] ; then
         if [ $source/$file -nt $target/$file ] ; then
-        echo "Newer file detected, copying..."
-        cp -r $source/$file $target/$file
+            echo "Newer file detected, copying to $backup_dir..."
+            scp -r $source/$file $backup_dir/$file
+            echo "Copying $file to $target..."
+            scp -r $source/$file $target/$file
         else
-        echo "File $file exists, skipping."
+            echo "File $file exists, skipping."
         fi
     else
-    echo "$file is being copied over to $target"
-    cp -r $source/$file $target/$file
+        echo "$file is being copied over to $target"
+        scp -r $source/$file $target/$file
     fi
 done
